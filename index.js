74ebb1acfb46bf816e67b093ecbb5f485098210b
Eric Kaufmann <ekaufmann@ad.corp.google.com>
2018-02-14 10:24:49 -0600

    Completed Homework

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
-// Your code goes here: event.preventDefault() $("img").attr("src")
-let thumbnailAttribute = function(event) {
+// Your code goes here:
 
-  event.preventDefault()
-  let thumbnailClick = jQuery(this)
-  jQuery("#main-image"),attr("src", thumbnailClick)
+jQuery( function(){
+
+  let thumbnailAttribute = function(event) {
+    $imgEle = jQuery(this);
+    img_src = $imgEle.attr("src");
+
+    let mainImg = jQuery("#main-image");
+    mainImg.attr("src",img_src);
+
 }
 
-jQuery("#thumbnails").on("click",thumbnailAttribute)
+jQuery("#thumbnails img").on("click",thumbnailAttribute)
+
+});
