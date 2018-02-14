46a0a80f5cb68f8fb5d2690305b0751a473e7403
Eric Kaufmann <ekaufmann@ad.corp.google.com>
2018-02-14 09:05:22 -0600

    JS page

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// Your code goes here:
+// Your code goes here: event.preventDefault() $("img").attr("src")
+let thumbnailAttribute = function(event) {
+
+  event.preventDefault()
+  let thumbnailClick = jQuery(this)
+  jQuery("#main-image"),attr("src", thumbnailClick)
+}
+
+jQuery("#thumbnails").on("click",thumbnailAttribute)
